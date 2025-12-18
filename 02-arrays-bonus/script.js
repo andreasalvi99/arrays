cb0ebcea281e5cc7790a89781a2d4f6830dfcb0f
andreasalvi99 <andrea.salvi.r@gmail.comm>
2025-12-18 20:11:36 +0100

    5th bonus task done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -33,4 +33,5 @@ const isFabioPresent = isFabioIn;
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join(", ");
+console.log(teachersString);
